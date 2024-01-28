9f8428dcc1853fdafce653b41b67764813ef1ea4
yourbestestfriend <151974151+yourbestestfriend@users.noreply.github.com>
2024-01-27 21:30:05 -0500

    created page two

--- src/pages/page_two.tsx
+++ src/pages/page_two.tsx
@@ -2,6 +2,25 @@ import Header from "../components/Header";
 
 export default function page_two() {
     return (
-        <h2>Page two page</h2>
+        <div className="Wavy-Image">
+            <img className="Full-Page-Image" src="wavypics.png"/>
+            <div className="note">
+                <div className="container">
+                    <h2 className="How-To-Use">How to Use it?</h2>
+                    <p className="noted">Your instruction in using AI will now begin. Below you will find<br />a common AI text generator. Enter the following commands <br />and observe the results:
+                        <br />-
+                        <br />-
+                        <br />-
+                        <br />-
+                        <br />As you can see, AI is a very powerful tool. However, it can be used 
+                        <br />for evil as well as good. 
+                    </p>
+                    <img className="cubes" src="cubes.png"></img>
+                    <div className="Home-Button-Container"><button className="Page-Button1">Next </button></div>
+                </div>
+            </div>
+        </div>
+        
+        
     )
 }
