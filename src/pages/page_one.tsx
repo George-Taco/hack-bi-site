3f4a0a7a7fa79d2cb905fb878ca937c91ec4afc3
EpicSauce54321 <chasemhochstein@gmail.com>
2024-01-28 00:00:44 -0500

    basics for what is ai

--- src/pages/page_one.tsx
+++ src/pages/page_one.tsx
@@ -3,10 +3,24 @@ import Header from "../components/Header";
 export default function page_one() {
     return (
         <div className="PageOne">
-       
+       <div className="SpaceParent">
+        <div className="HeaderContainer">
         <h1 className="PageOneHeader">What is AI?</h1>
-        <div className="Card1"></div>
-        <div className="Card2"></div>
+        </div>
+        </div>
+        <div className="SpaceParent">
+        <div className="Card1">
+           <p className="PageOneText">To begin the briefing: A short review of Artificial Intelligence. 
+            Early research into AI began circa 1950 AD by mathematician Alan Turing. From there it developed rapidly until it became integrated into almost every aspect of everyday life.
+            </p>
+        </div>
+        </div>
+       <div className="SpaceParent">
+        <div className="Card2">
+            <p className="PageOneText2">In order to use AI responsibly, it is important to know how it works. To keep it simple: AI is like a very bright student. In developing AI, the AI model, or student, is provided with a large library from which to learn from. The model memorizes the information from the library, and then the teacher gives the model new information and asks the model to compare it to the library. 
+            </p>
+        </div>
+        </div>
         </div>
     )
 }
