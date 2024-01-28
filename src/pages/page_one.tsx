fc2fcef27a89fa6bdea3929844c55cdeee083c0e
Chase Hochstein <147351940+EpicSauce54321@users.noreply.github.com>
2024-01-27 20:39:48 -0500

    Revert "added the layout for page 1"

--- src/pages/page_one.tsx
+++ src/pages/page_one.tsx
@@ -2,11 +2,6 @@ import Header from "../components/Header";
 
 export default function page_one() {
     return (
-        <div className="PageOne">
-       
-        <h1 className="PageOneHeader">What is AI?</h1>
-        <div className="Card1"></div>
-        <div className="Card2"></div>
-        </div>
+        <h2>page one</h2>
     )
 }
