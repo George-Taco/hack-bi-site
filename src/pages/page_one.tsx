307d2df825c434cdd06901a9027153a9a524c2da
George-Taco <saorotmg@gmail.com>
2024-01-28 03:15:14 -0500

    Reformatting changes

--- src/pages/page_one.tsx
+++ src/pages/page_one.tsx
@@ -3,12 +3,13 @@ import Header from "../components/Header";
 export default function page_one() {
     return (
         <div className="PageOne">
-       <div className="SpaceParent">
+        <div className="SpaceParent">
         <div className="HeaderContainer">
-        <h1 className="PageOneHeader">What is AI?</h1>
+        <div className="Header-Container1">
+            <h1 className="PageOneHeader">What is AI?</h1></div>
         </div>
         <div className="ButtonParent">
-        <div className="Home-Button-Container-ONE">
+        <div className="Button-Container1">
             <a href="page_two">
             <button className="Page-Button1">Next →</button>
             </a>
@@ -17,6 +18,7 @@ export default function page_one() {
         </div>
         <div className="SpaceParent">
         <div className="Card1">
+            <div className="Card-Title1"><h1>History</h1></div>
            <p className="PageOneText">To begin the briefing: A short review of Artificial Intelligence. 
             Early research into AI began circa 1950 AD by mathematician Alan Turing. From there it developed rapidly until it became integrated into almost every aspect of everyday life.
             </p>
@@ -24,6 +26,7 @@ export default function page_one() {
         </div>
        <div className="SpaceParent">
         <div className="Card2">
+            <div className="Card-Title2"><h1>How it works</h1></div>
             <p className="PageOneText2">In order to use AI responsibly, it is important to know how it works. To keep it simple: AI is like a very bright student. In developing AI, the AI model, or student, is provided with a large library from which to learn from. The model memorizes the information from the library, and then the teacher gives the model new information and asks the model to compare it to the library. </p>
         </div>
         </div>
