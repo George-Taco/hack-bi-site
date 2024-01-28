039598ed13b39f3fa2d44870dbc9ba93a9b4d7bf
EpicSauce54321 <chasemhochstein@gmail.com>
2024-01-28 05:35:26 -0500

    Update page_two.tsx

--- src/pages/page_two.tsx
+++ src/pages/page_two.tsx
@@ -1,23 +1,23 @@
 import Header from "../components/Header";
-
+import link from 
 export default function page_two() {
     return (
         <div className="Wavy-Image">
-            <img className="Full-Page-Image" src="wavypics.png"/>
+            <img className="Full-Page-Image" src="wavypics.png" alt="waves"/>
             <div className="note">
                 <div className="container">
                     <h2 className="How-To-Use">How to Use it?</h2>
-                    <p className="noted">Your instruction in using AI will now begin. Below you will find<br />a common AI text generator. Enter the following commands <br />and observe the results:
-                        <br />-
-                        <br />-
-                        <br />-
-                        <br />-
+                    <p className="noted">Your instruction in using AI will now begin. <br />Enter the following commands <br />and observe the results:
+                        <br />Write me an essay
+                        <br />Solve a math problem
+                        <br />Brainstorm a slideshow topic
+                        <br />Write code for me
                         <br />As you can see, AI is a very powerful tool. However, it can be used 
-                        <br />for evil as well as good. 
+                        <br />for evil as well as good.
                     </p>
-                    <img className="cubes" src="cubes.png"></img>
+                    <img className="cubes" src="cubes.png" alt="cubes"></img>
                     <div className="Home-Button-Container"><a href="https://inspiring-empanada-88316d.netlify.app/"><button className="Page-Button1">Next →</button></a></div>
-                    <img className="astro" src="astro.png"></img>
+                    <img className="astro" src="astro.png" alt="stars"></img>
                 </div>
             </div>
         </div>
